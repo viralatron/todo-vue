8ffa577cc3ed67dad10dea7b806c4842ad9d9fd6
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2023-04-18 11:16:20 -0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useHead } from "#imports";
+useHead({
+  title: "todo list app",
+});
+</script>
+<template>
+  <div>
+    <h1>My To-Do List</h1>
+    <ul>
+      <li>tarefa 1</li>
+      <li>tarefa 2</li>
+      <li>tarefa 3</li>
+    </ul>
+  </div>
+</template>
